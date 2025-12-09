337cdaa242f907af9d1e0f423f4e6f80d84bbcb5
sophie-de-jong <dejongmsophie@gmail.com>
2025-12-09 16:11:00 -0500

    Update dotfiles: Dec 09, 2025 04:11:00 PM

--- fish/conf.d/rustup.fish
+++ fish/conf.d/rustup.fish
@@ -1,2 +1,1 @@
 sh "$XDG_DATA_HOME/cargo/env"
-source "/home/sophie/.local/share/cargo/env.fish"
--- fish/config.fish
+++ fish/config.fish
@@ -15,6 +15,7 @@ set -gx NAME "Sophie de Jong"
 set -gx EMAIL "dejongmsophie@gmail.com"set -gx XDG_CONFIG_HOME $HOME/.config
 set -gx VISUAL helix
 set -gx EDITOR $VISUAL
+set -gx XDG_CONFIG_HOME $HOME/.config
 set -gx XDG_CACHE_HOME $HOME/.cache
 set -gx XDG_DATA_HOME $HOME/.local/share
 set -gx XDG_BIN_HOME $HOME/.local/bin
