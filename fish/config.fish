5b1cb947f034078065d1dbcad90fda9dbcfb1ed7
sophie-de-jong <dejongmsophie@gmail.com>
2025-12-21 13:43:36 -0500

    Update dotfiles: Dec 21, 2025 01:43:36 PM

--- fish/config.fish
+++ fish/config.fish
@@ -43,7 +43,6 @@ set -gx CUDA_CACHE_PATH $XDG_CACHE_HOME/nv
 alias ls 'eza --icons'
 alias ll 'eza --long --all --icons --git --header'
 alias lt 'eza --long --all --icons --git --header --tree'
-alias cat 'bat --paging=never'
 alias c wl-copy
 alias p wl-paste
 alias neofetch hyfetch
